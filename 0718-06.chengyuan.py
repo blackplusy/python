db03a6dd77e94a7154961c01bfe1ea4fff0086cd
blackplusy <happyming07@sina.com>
2018-07-18 17:39:21 +0800

    Add files via upload

---
+++ 0718-06.chengyuan.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+name='helloworld'
+for i in name:
+	print(i)
+
+li=['heygor','handsome']
+for a in li:
+	print(a)
+
+tup=(1,2,3)
+for i in tup:
+	print(i)
+
+if 'e' in name:
+	print('im here!!')
