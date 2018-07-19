cc9ea8fd4dfe8a242b65a49e79d8d77d3f6afaab
blackplusy <happyming07@sina.com>
2018-07-19 15:37:03 +0800

    Add files via upload

---
+++ 0719-06.string.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+'''
+name='baidu'
+print(name[0]) #b
+print(name[-5])#b
+print(name[4])#u
+'''
+'''
+a='www.baidu.com'
+print(a[4:]) #baidu.com 第5个字符到最后一个字符
+print(a[:-4])#www.baidu 直到倒数第四个字符(之前)
+print(a[4:-4])#baidu    第5个字符到倒数第四个字符
+
+'''
+'''
+a='python'
+b='no.1'
+print(a+b)
+print(a[0]+b[2:])
+'''
+a='hello'
+if 'e' in a:
+	print('wozai')
