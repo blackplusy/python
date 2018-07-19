e24af6afe6e1782a2b89abe8602516a926bc0263
blackplusy <happyming07@sina.com>
2018-07-19 11:57:42 +0800

    Add files via upload

---
+++ 0719-04.for.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+
+str1='hello'#字符串
+for i in str1:
+	print(i)
+
+tup=('heygor','ladeng') #元组
+for a in tup:
+	print(a)
+
+
+#函数range()
+#range(10) 从0开始，0-9
+#range(1,10) 从1开始，1-9
+print('******************')
+for i in range(10):
+	print(i)
+
+for i in range(1,10):
+	print(i)
---
+++ 0719-05.while.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+a=0
+#满足条件后执行，不满足条件不执行
+while a<10:
+	a+=1
+	if a==6:
+#		continue
+		break
+	print(a)
+
+#死循环
+#while 1
+#while True
