4f88e9b3bd25ae25fec4fd26b3367fcd3fe17b69
blackplusy <happyming07@sina.com>
2018-07-20 15:11:48 +0800

    Add files via upload

---
+++ 0720-04.file.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+'''
+file=open('/opt/passwd','r')
+for i in file:
+	print(i)
+file.close()
+'''
+
+str1='hello baby!!'
+
+file=open('/home/heygor/test1','w')
+file.write(str1)
+file.close()
