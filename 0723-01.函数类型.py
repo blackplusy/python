37b7965958539de7c3a547b87db4106c9c621791
blackplusy <happyming07@sina.com>
2018-07-23 11:28:46 +0800

    Add files via upload

---
+++ 0723-01.函数类型.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+def hello():
+	print('hello world!')
+
+hello()
+
+
+def sub(x,y):
+	print('x+y=',x+y)
+
+sub(2,3)
+
+
+def sleep():
+	return 'sleep'
+s=sleep()
+print(s)
+
+def sub(x,y):
+	return x+y
+
+res=sub(10,20)
+print(res)
+
