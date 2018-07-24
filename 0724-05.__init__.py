81dda1354a5236852ac593cfa559c3bd2b6ecbf4
blackplusy <happyming07@sina.com>
2018-07-24 17:25:50 +0800

    Add files via upload

---
+++ 0724-05.__init__.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+'''
+class student:
+	def __init__(self):
+		self.boy=20
+		self.girl=30
+	def study(self):
+		print('god god study')
+
+simida=student()
+print(simida.boy)
+print(simida.girl)
+simida.study()
+
+'''
+class student:
+	def __init__(self,boy,girl):
+		self.badboy=boy
+		self.cutegirl=girl
+	def study():
+		print('study!!!')
+z=student(20,30)
+print('班级中男生是:%d'% z.badboy)
+print('班级中女生是:%d'% z.cutegirl)
